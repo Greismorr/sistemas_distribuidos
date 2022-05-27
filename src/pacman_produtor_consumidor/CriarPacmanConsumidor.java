5630d63777fb7856525d6be2881227052fa3dd0c
greismorr <gabriel-reis1999@hotmail.com>
2022-05-27 00:35:02 -0300

    PacmanConsumidor buscando objeto remoto numa URL

--- src/pacman_produtor_consumidor/CriarPacmanConsumidor.java
+++ src/pacman_produtor_consumidor/CriarPacmanConsumidor.java
@@ -7,19 +7,19 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class CriarPacmanConsumidor {
-    public static void main(String[] args) throws UnknownHostException {  
-    	
+    public static void main(String[] args) throws UnknownHostException { 
+        final String HOST = "localhost";
         final String BUFFER = "BUFFER_DE_FANTASMAS";
         
     	try {        
-            Registry registry = LocateRegistry.getRegistry(1099);
+            Registry registry = LocateRegistry.getRegistry(HOST, 1099);
             BufferInterface buffer = (BufferInterface)registry.lookup(BUFFER);
             
             PacmanConsumidor pacman = new PacmanConsumidor(buffer);
 
             pacman.start(); 
         } 
-    	catch ( RemoteException | NotBoundException  e) {
+    	catch (RemoteException | NotBoundException  e) {
             System.err.println("ProdutorConsumidor exception: " + e.getMessage());
         }
     }
