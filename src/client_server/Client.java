53a1e42dbee895e4f1d895b1bc5ae59d9ba809bf
elayne <elayne.natalia@outlook.com>
2022-05-28 21:48:27 -0300

    criando jar

---
+++ src/client_server/Client.java
@@ -0,0 +1,31 @@
+package client_server;
+
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import pacman_produtor_consumidor.BufferInterface;
+import pacman_produtor_consumidor.PacmanConsumidor;
+import properties.Manipulador;
+
+public class Client {
+
+	public static void main(String args[]) throws UnknownHostException {
+        
+		try {      
+    		
+            Manipulador.CarregarDadosSensiveis();
+    		
+            Registry registry = LocateRegistry.getRegistry(Manipulador.getUrl(), Manipulador.getPort());
+            BufferInterface buffer = (BufferInterface)registry.lookup(Manipulador.getBuffer().replace("/", ""));
+            		
+            PacmanConsumidor pacman = new PacmanConsumidor(buffer);
+
+            pacman.start(); 
+        } 
+    	catch (RemoteException | NotBoundException  e) {
+            System.err.println("ProdutorConsumidor exception: " + e.getMessage());
+        }
+	}
+}
